52b45be8d1dee76e10f6f4520f7d7823db02d67a
Michael Maximenko <Mishamax2007@gmail.com>
2021-12-28 22:26:30 +0200

    Updated

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const config = require('config');
- 
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -27,27 +27,38 @@ const userSchema = new mongoose.Schema({
     type: Boolean,
     required: true
   },
-  createdAt: { type: Date, default: Date.now }
+  createdAt: { type: Date, default: Date.now },
+  cards: Array
 });
- 
+
 userSchema.methods.generateAuthToken = function () {
   const token = jwt.sign({ _id: this._id, biz: this.biz }, config.get('jwtKey'));
   return token;
 }
- 
+
 const User = mongoose.model('User', userSchema);
- 
+
 function validateUser(user) {
- 
+
   const schema = Joi.object({
     name: Joi.string().min(2).max(255).required(),
     email: Joi.string().min(6).max(255).required().email(),
     password: Joi.string().min(6).max(1024).required(),
     biz: Joi.boolean().required()
   });
- 
+
   return schema.validate(user);
 }
- 
+
+function validateCards(data) {
+
+  const schema = Joi.object({
+    cards: Joi.array().min(1).required()
+  });
+
+  return schema.validate(data);
+}
+
 exports.User = User;
 exports.validate = validateUser;
+exports.validateCards = validateCards;
